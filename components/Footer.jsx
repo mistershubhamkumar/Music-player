189b881e15c717f9c1398f5df9e0df0d58711f80
Shubham Kumar <kumaruikeshubham@gmail.com>
2025-03-12 20:35:33 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <footer className="py-10 mt-10 text-center">
-            <p className="text-sm">Made ❤️ by <a className="hover:underline hover:text-primary" href="">Oyy Sreejan</a><span className="text-primary">.</span></p>
+            <p className="text-sm">Made by ❤️ 𝙈𝙨𝙧_🅢𝙝𝙪𝙗𝙝𝙖𝙢 <a className="hover:underline hover:text-primary" href="">Oyy 🅢𝙝𝙪𝙗𝙝𝙖𝙢</a><span className="text-primary">.</span></p>
             <ul className="flex gap-1 items-center justify-center mt-1">
-                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://instagram.com/sreejan_edit" target="_blank">Instagram</a></li>
-                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://wa.me/message/GET4BX5JRTBQK1" target="_blank">Whatsapp</a></li>
+                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://instagram.com/msr_shubham.k" target="_blank">Instagram</a></li>
+                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://wa.me/message/916260273863" target="_blank">Whatsapp</a></li>
             </ul>
         </footer>
     )
